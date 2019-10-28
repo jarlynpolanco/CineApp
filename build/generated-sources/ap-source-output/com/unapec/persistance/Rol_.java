95d2e153e1d4952d4fce0543f57e2565eac6d118
Jarlyn Manuel Polanco De la Rosa <jarlynpolanco@gmail.com>
2019-10-28 19:55:57 -0400

    New changes

--- build/generated-sources/ap-source-output/com/unapec/persistance/Rol_.java
+++ build/generated-sources/ap-source-output/com/unapec/persistance/Rol_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-28T19:39:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-28T19:50:38")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
